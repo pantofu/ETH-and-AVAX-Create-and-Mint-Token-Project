d09c3cf491b5a769abe922bb0f5ddb6e064649a8
Jester Cruzate <104056079+pantofu@users.noreply.github.com>
2024-07-10 08:26:42 +0800

    Update Functions.sol

--- Functions.sol
+++ Functions.sol
@@ -1,1 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract ExMachinaToken is ERC20, Ownable {
+    constructor(uint256 initialSupply, uint8 decimals, string memory name, string memory symbol) ERC20(name, symbol) Ownable(msg.sender) {
+        _mint(msg.sender, initialSupply * (10 ** uint256(decimals)));
+    }
+    
+    // Only the contract owner can mint new tokens
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+    
+    // Any user can burn their tokens
+    function burn(uint256 amount) public {
+        _burn(msg.sender, amount);
+    }
+}
