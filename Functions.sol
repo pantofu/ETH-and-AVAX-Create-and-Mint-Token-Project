8354b7fb81a51cef1b052e7697f7db7441b4045e
Jester Cruzate <104056079+pantofu@users.noreply.github.com>
2024-07-17 23:55:36 +0800

    Update Functions.sol

--- Functions.sol
+++ Functions.sol
@@ -18,4 +18,8 @@ contract ExMachinaToken is ERC20, Ownable {
     function burn(uint256 amount) public {
         _burn(msg.sender, amount);
     }
+
+    function transfer(address recipient, uint256 amount) public override returns (bool) {
+        return super.transfer(recipient, amount);
+    }
 }
